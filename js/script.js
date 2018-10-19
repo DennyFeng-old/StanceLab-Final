909d561181a7c875f922720dd2d9bf749a80d290
feng <feng@FENG-PC>
2018-10-18 17:43:39 -0700

    StanceLab
    StanceLab rework

---
+++ js/script.js
@@ -0,0 +1,16 @@
+ /* Maps */
+    var map = new GMaps({
+        div: 'map'
+        , lat: 37.6412134,
+         lng: -122.4146704
+        , zoom: 12
+    });
+    map.addMarker({
+        lat: 37.6412134,
+         lng:  -122.4146704
+        , title: '220 S Linden Ave, South San Francisco, CA 94080'
+        , infoWindow: {
+            content: '220 S Linden Ave, South San Francisco, CA 94080'
+        }
+    });
+//   Map end 
